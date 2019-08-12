9fe2deff04b43e670bebc6a858600f4e1c8be600
Koji Nakagawa <lightupthesky0627@gmail.com>
2019-08-11 19:40:53 -0700

    Added displayed temporarily  clusteres

--- src/components/Map/MapComponent.js
+++ src/components/Map/MapComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import MarkerClusterer from '@google/markerclusterer';
 
 class MapComponent extends Component {
     constructor(props) {
@@ -18,13 +19,70 @@ class MapComponent extends Component {
         this.setCurrentLocation();
     }
 
+    async handleApiLoaded(map, maps) {
+        const google = window.google;
+
+        // const communityLists = await getCommunityList(currentPosition, distance);
+
+        var communityLists = [
+            { lat: -31.563910, lng: 147.154312 },
+            { lat: -33.718234, lng: 150.363181 },
+            { lat: -33.727111, lng: 150.371124 },
+            { lat: -33.848588, lng: 151.209834 },
+            { lat: -33.851702, lng: 151.216968 },
+            { lat: -34.671264, lng: 150.863657 },
+            { lat: -35.304724, lng: 148.662905 },
+            { lat: -36.817685, lng: 175.699196 },
+            { lat: -36.828611, lng: 175.790222 },
+            { lat: -37.750000, lng: 145.116667 },
+            { lat: -37.759859, lng: 145.128708 },
+            { lat: -37.765015, lng: 145.133858 },
+            { lat: -37.770104, lng: 145.143299 },
+            { lat: -37.773700, lng: 145.145187 },
+            { lat: -37.774785, lng: 145.137978 },
+            { lat: -37.819616, lng: 144.968119 },
+            { lat: -38.330766, lng: 144.695692 },
+            { lat: -39.927193, lng: 175.053218 },
+            { lat: -41.330162, lng: 174.865694 },
+            { lat: -42.734358, lng: 147.439506 },
+            { lat: -42.734358, lng: 147.501315 },
+            { lat: -42.735258, lng: 147.438000 },
+            { lat: -43.999792, lng: 170.463352 }
+        ];
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var markers = communityLists.map(function (location, i) {
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length],
+                draggable: true,
+                title: "hello"
+            });
+        });
+        var markerCluster = new MarkerClusterer(map, markers,
+            { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+        
+
+        // const circle = new google.maps.Circle({
+        //     strokeColor: "#FF0000",
+        //     strokeOpacity: 0.8,
+        //     strokeWeight: 2,
+        //     fillColor: "#FF0000",
+        //     fillOpacity: 0.3,
+        //     map,
+        //     center: this.state.center,
+        //     radius: 100
+        // });
+    };
+
     render() {
         return(
             <div style={{ height: '100vh', width: '100%'}}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP }}
-                    defaultCenter={this.state.center}
-                    defaultZoom={this.state.zoom}
+                    center={this.state.center}
+                    zoom={this.state.zoom}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps}) => this.handleApiLoaded(map, maps)}
                 >
                 </GoogleMapReact>
             </div>
@@ -36,10 +94,12 @@ class MapComponent extends Component {
             navigator.geolocation.getCurrentPosition((position) => {
                 this.setState({
                     center: {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
+                        // lat: position.coords.latitude,
+                        // lng: position.coords.longitude,
+                        lat: -28.024,
+                        lng: 140.887
                     },
-                    zoom: 14
+                    zoom: 3
                 });
             });
         } else {
